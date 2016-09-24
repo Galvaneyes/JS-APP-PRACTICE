9fbb0c380dff901676b6781f8e9b2bdc86173673
Galvaneyes <m.v.manchev@gmail.com>
2016-09-24 15:49:17 +0300

    Add bullshits

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict'
+function solver() {
+    console.log('asjdkjasd');
+    
+    function sum(a,b) {
+        return a + b;
+    }
+    
+    console.log(sum(10,42));
+    
+}
+
+let text = solver();
