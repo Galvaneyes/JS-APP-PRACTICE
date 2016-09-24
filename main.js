541a1936939fbe0f7262ded5c1e7b960729a0b67
Galvaneyes <m.v.manchev@gmail.com>
2016-09-24 19:36:29 +0300

    New trash for trash

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 window.onload = function() {
    
     function printText() {
-        console.log($('#search').val());          
+        let user = $('#search').val();
+        console.log(user);
+
+        let promise = new Promise();
+
+        
+        $.get('registred/' + user);
+
+        $('#search').val('');        
+
     }
 
     $('#go').on('click', printText);
 };  
+
+function printUser(user) {
+    console.log('This is ' + user);
+}
+
+function printError() {
+    console.log('Fail!');
+}
