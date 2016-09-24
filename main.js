a5ac1c9b8d2fdca00376994f05938f93f212e9c8
Adrian <adrian.bozhankov@gmail.com>
2016-09-24 19:31:44 +0300

    Added some trash js code

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
 window.onload = function() {
-   
-    function printText() {
-        console.log($('#search').val());          
+	var url = "https://restcountries.eu/rest/v1/all";
+
+	function printText() {
+		getAllTowns(printTowns);
+	}
+
+    function getAllTowns(success) {
+        $.ajax({
+            method: "GET",
+            url: url,
+            success: success
+        });
     }
 
-    $('#go').on('click', printText);
-};  
+	function printTowns (data) {
+		var startsWith = $('#search').val();
+		var towns = [];
+
+		for (var town in data) {
+			towns.push(data[town].name);
+		}
+		var townsToDisplay = [];
+		for (var i = 0; i < towns.length; i++) {
+			if (town.toLowerCase().substring(0, startsWith.length) == startsWith.toLowerCase()) {
+				townsToDisplay.push(towns[i]);
+			}
+		}
+
+		console.log(townsToDisplay);
+
+		townsToDisplay.forEach((x) => $('#inputText').append(x));
+	}
+
+	function getData(data) {
+		$.get(url, data, success);
+	}
+
+	$('#search').on('input', printText);
+};
